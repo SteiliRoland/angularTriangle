8daa4805a4addd415e577dfb27aaaaf0e436b748
Steili Roland <steiliroland2001@gmail.com>
2022-01-13 10:11:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'triangle';
+  base: string;
+  height: number;
+  area: number;
+  constructor(){
+    this.base = '';
+    this.height = 0;
+    this.area = 0;
+  }
+  calcArea(): any{
+    console.log(this.base);
+    this.area = Number(this.base) * Number(this.height) /2;
+    this.base='';
+    this.height=0;
+  }
 }
